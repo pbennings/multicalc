8578ef5df4d531abaf959570e77ab18980d92477
Paul Bennings <paul.bennings@codeimmersives.com>
2019-03-13 12:52:44 -0400

    finished project

--- main.js
+++ main.js
@@ -12,34 +12,29 @@ window.onload = init;
 
 // Sets up event listeners
 function init() {
+  // When the user clicks the append button, append the given number to the list.
+  document.querySelector("#append").addEventListener("click", appendToList);
 
-    // When the user clicks the append button, append the given number to the list.
-    document.querySelector('')
-        .addEventListener('click', appendToList);
+  // When the user clicks the remove button, remove the number at the index given from the list.
 
-    // When the user clicks the remove button, remove the number at the index given from the list.
-    document.querySelector('')
-        .addEventListener('click', removeFromList);
+  document.querySelector("#remove").addEventListener("click", removeFromList);
 
-    // When the user clicks the clear button, remove all items from the list.
-    document.querySelector('')
-        .addEventListener('click', clearList);
+  // When the user clicks the clear button, remove all items from the list.
+  document.querySelector("#clear").addEventListener("click", clearList);
 
-    // When the user clicks the add button, add the value to each item.
-    document.querySelector('')
-        .addEventListener('click', addToAll);
+  // When the user clicks the add button, add the value to each item.
+  document.querySelector("#add").addEventListener("click", addToAll);
 
-    // When the user clicks the add button, add the value to each item.
-    document.querySelector('')
-        .addEventListener('click', subtractFromAll);
-        
-    // When the user clicks the multiply button, multiply the value by each item.
-    document.querySelector('')
-        .addEventListener('click', multiplyByAll);
-        
-    // When the user clicks the divide button, divide the value from each item.
-    document.querySelector('')
-        .addEventListener('click', divideFromAll);
+  // When the user clicks the add button, add the value to each item.
+  document
+    .querySelector("#subtract")
+    .addEventListener("click", subtractFromAll);
+
+  // When the user clicks the multiply button, multiply the value by each item.
+  document.querySelector("#multiple").addEventListener("click", multiplyByAll);
+
+  // When the user clicks the divide button, divide the value from each item.
+  document.querySelector("#divide").addEventListener("click", divideFromAll);
 }
 
 /*
@@ -50,31 +45,39 @@ function init() {
 
 // Append to the list.
 function appendToList(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
+  // Make sure page doesn't reload on button press.
+  event.preventDefault();
 
-    // Get the value we're going to append from the input field.
-    let number = document.querySelector('').value;
+  // Get the value we're going to append from the input field.
+  let number = document.querySelector("#list-number").value;
 
-    // Append the number to our array.
-    // Hint: here (and elsewhere), watch the TYPE of the value above.
-    // Research `typeof` operator if you're not sure.
+  // Append the number to our array.
+  // where you want to put it into; action; where it's coming from.
 
-    // Update our html.
+  // parseInt will convert the string to a number;
 
+  let convertNum = parseInt(number, 10);
+  // push adds the value to the end of the array;
+  numbers.push(convertNum);
+
+  // Hint: here (and elsewhere), watch the TYPE of the value above.
+  // Research `typeof` operator if you're not sure.
+
+  // Update our html.
+  addToUL(convertNum);
 
 }
 // Remove from the list.
 function removeFromList(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
+  // Make sure page doesn't reload on button press.
+  event.preventDefault();
 
-    // Get the index we'll remove from the input field.
-    let index = document.querySelector('').value;
+  // Get the index we'll remove from the input field.
+  let index = document.querySelector("").value;
 
-    // Remove the number at that index from the list.
+  // Remove the number at that index from the list.
 
-    /*
+  /*
         ### Hints:
 
         * You can either loop through... or use array methods. Check out concat!
@@ -84,20 +87,16 @@ function removeFromList(event) {
         tools.
     */
 
-    // Update our html.
-
-
+  // Update our html.
 }
 
 function clearList(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
+  // Make sure page doesn't reload on button press.
+  event.preventDefault();
 
-    // Clear the array of all values.
+  // Clear the array of all values.
 
-
-    // Update our html.
-    
+  // Update our html.
 }
 
 /*
@@ -106,63 +105,53 @@ function clearList(event) {
 # ========================================================
 */
 
-
 function addToAll(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
+  // Make sure page doesn't reload on button press.
+  event.preventDefault();
 
-    // Grab value to add.
-    let numberToAdd = document.querySelector('').value;
+  // Grab value to add.
+  let numberToAdd = document.querySelector("").value;
 
-    // Add value to everything on the list.
+  // Add value to everything on the list.
 
-
-    // Update our html.
-
+  // Update our html.
 }
 
 function subtractFromAll(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
-    
-    // Grab value to subtract.
-    let numberToSubtract = document.querySelector('').value;
-    
-    // Subtract value from everything on the list.
-  
+  // Make sure page doesn't reload on button press.
+  event.preventDefault();
 
-    // Update our html.
+  // Grab value to subtract.
+  let numberToSubtract = document.querySelector("").value;
 
+  // Subtract value from everything on the list.
+
+  // Update our html.
 }
 
 function multiplyByAll(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
-    
-    // Grab value to multiply.
-    let numberToMultiply = document.querySelector('').value;
-    
-    // Multiply value by everything on the list.
-    
-    
-    // Update our html.
-    
+  // Make sure page doesn't reload on button press.
+  event.preventDefault();
+
+  // Grab value to multiply.
+  let numberToMultiply = document.querySelector("").value;
+
+  // Multiply value by everything on the list.
+
+  // Update our html.
 }
 
 function divideFromAll(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
+  // Make sure page doesn't reload on button press.
+  event.preventDefault();
 
-    // Grab value to add.
-    let numberToDivide = document.querySelector('').value;
+  // Grab value to add.
+  let numberToDivide = document.querySelector("").value;
 
-    // Divide value from everything on the list.
-    
+  // Divide value from everything on the list.
 
-    // // Update our html.plo
-    
+  // // Update our html.plo
 }
-
 
 /*
 # ========================================================
@@ -171,23 +160,28 @@ function divideFromAll(event) {
 */
 
 function updateUL() {
-    clearUL();
-    for(let i = 0; i < numbers.length; i++) {
-        addToUL(numbers[i]);
-    }
+  clearUL();
+  for (let i = 0; i < numbers.length; i++) {
+    addToUL(numbers[i]);
+  }
 }
 
 function clearUL() {
-    const ul = document.querySelector('');
-    while(ul.hasChildNodes()) {
-        ul.removeChild(ul.firstChild);
-    }
+  const ul = document.querySelector("#number-list");
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
 }
 
 // Append to the UL.
 function addToUL(numberToAppend) {
-    const ul = document.querySelector('');
-    const newLI = document.createElement('');
-    newLI.innerText = numberToAppend;
-    UL.appendChild(newLI);
+  // locate id of number-list in your html
+  const UL = document.querySelector("#number-list");
+  // creates a new element of li inside the HMTL
+  const newLI = document.createElement("li");
+  // inside the new element putting text numberToAppend;
+  newLI.innerText = numberToAppend;
+  // add to the unordered list a new li under it
+  // UL is the parent, li is the child
+  UL.appendChild(newLI);
 }
